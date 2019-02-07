73836c21597c93ff021d99ae8b96ae28e915f5a2
Adam Heaton <barnesheaton@gmail.com>
2019-02-07 15:18:15 -0700

    Reveal words used for sentiment calculation
     - Styling

--- src/LandingView.js
+++ src/LandingView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Sentiment from "sentiment";
+import ReactMarkdown from "react-markdown";
 
 import "./LandingView.css";
 import Comment from "./comment.js";
@@ -7,11 +8,13 @@ import comments from "./assets/comments.js";
 
 export default class Component extends React.Component {
   state = {
-    positiveComments: [],
+    comments: [],
     neutralComments: [],
     negativeComments: [],
-    comments: [],
+    positiveComments: [],
     results: [],
+    revealTerms: true,
+    searchTime: null,
     value: ""
   };
 
@@ -41,133 +44,179 @@ export default class Component extends React.Component {
     return (
       <div className="container">
         <header className="header">
-          <p>Search through comments by comma separated keywords</p>
+          <p className="header">
+            Search through comments by comma separated keywords
+          </p>
           <div>
-            <input
-              placeholder="stryd, awesome, running"
-              className="input"
-              type="text"
-              value={this.state.value}
-              onChange={event => {
-                this.onChange(event);
-              }}
-            />
-            <button className="button" onClick={() => this.onSearch()}>
-              Search
-            </button>
+            <div className="input-container">
+              <form onSubmit={event => this.onSearch(event)}>
+                <input
+                  placeholder="stryd, awesome, running"
+                  className="input"
+                  type="text"
+                  value={this.state.value}
+                  onChange={event => {
+                    this.onChange(event);
+                  }}
+                />
+              </form>
+              <button
+                className="button"
+                onClick={event => this.onSearch(event)}
+              >
+                Search
+              </button>
+              <button
+                className="button"
+                onClick={() =>
+                  this.setState({ revealTerms: !this.state.revealTerms })
+                }
+              >
+                Show Sentiment Words
+              </button>
+            </div>
+            {this.state.searchTime && (
+              <p>{`Got ${this.numberOfResults()} results in ${
+                this.state.searchTime
+              } milliseconds`}</p>
+            )}
           </div>
         </header>
-        <div className="results">
-          <div className="comments-container">
-            <p className="section-header">Positive Comments</p>
-            <div className="comments-total">
-              <p style={{ color: "white" }}>Total:</p>
-              <p style={{ color: "blue", "padding-left": 6 }}>
-                {this.state.positiveComments.length} (
-                {this.percentage(this.state.positiveComments.length)})%
-              </p>
-              <p style={{ "padding-left": 6 }} />
-            </div>
-            <div className="comments">
-              {this.state.positiveComments.map(comment => (
-                <Comment comment={comment} />
-              ))}
-            </div>
-          </div>
-          <div className="comments-container">
-            <p className="section-header">Neutral Comments</p>
-            <div className="comments-total">
-              <p style={{ color: "white" }}>Total:</p>
-              <p style={{ color: "blue", "padding-left": 6 }}>
-                {this.state.neutralComments.length} (
-                {this.percentage(this.state.neutralComments.length)})%
-              </p>
-              <p style={{ "padding-left": 6 }} />
-            </div>
-            <div className="comments">
-              {this.state.neutralComments.map(comment => (
-                <Comment comment={comment} />
-              ))}
-            </div>
-          </div>
-          <div className="comments-container">
-            <p className="section-header">Negative Comments</p>
-            <div className="comments-total">
-              <p style={{ color: "white" }}>Total:</p>
-              <p style={{ color: "blue", "padding-left": 6 }}>
-                {this.state.negativeComments.length} (
-                {this.percentage(this.state.negativeComments.length)})%
-              </p>
-              <p style={{ "padding-left": 6 }} />
-            </div>
-            <div className="comments">
-              {this.state.negativeComments.map(comment => (
-                <Comment comment={comment} />
-              ))}
-            </div>
-          </div>
-        </div>
+        {(this.state.neutralComments.length ||
+          this.state.negativeComments.length ||
+          this.state.positiveComments.length) &&
+          this.rendeResults()}
+        <ReactMarkdown
+          source={`
+          ${"```"}
+          js
+          var React = require('react');
+          var Markdown = require('react-markdown');
+
+          React.render(
+            <Markdown source="# Your markdown here" />,
+            document.getElementById('content')
+          );
+          ${"```"}
+          `}
+        />
       </div>
     );
   }
 
-  percentage(value) {
-    return Math.floor(
-      (value /
-        (this.state.positiveComments.length +
-          this.state.negativeComments.length +
-          this.state.neutralComments.length)) *
-        100
+  rendeResults() {
+    const sections = [
+      { title: "Positive", stateKey: "positiveComments" },
+      { title: "Neutral", stateKey: "neutralComments" },
+      { title: "Negative", stateKey: "negativeComments" }
+    ];
+
+    return (
+      <div className="results">
+        {sections.map(section => {
+          return (
+            <div className="comments-container">
+              <div className="section-header">
+                {section.title} Comments
+                <span style={{ color: "blue", "padding-left": 6 }}>
+                  {this.state[section.stateKey].length} (
+                  {this.percentage(this.state[section.stateKey].length)})%
+                </span>
+              </div>
+              <div className="comments">
+                {this.state[section.stateKey].map(comment => (
+                  <Comment
+                    comment={comment}
+                    revealTerms={this.state.revealTerms}
+                  />
+                ))}
+              </div>
+            </div>
+          );
+        })}
+      </div>
     );
   }
 
-  onSearch() {
-    if (this.state.value) {
-      this.filter();
-    } else {
-      window.alert("You must enter a search term");
-    }
+  filter() {
+    return new Promise(resolve => {
+      let positiveComments = [];
+      let neutralComments = [];
+      let negativeComments = [];
+      const keywords = this.state.value.replace(/ /, "").split(",");
+      this.state.comments
+        .filter(comment => {
+          for (var i = 0; i < keywords.length; i++) {
+            if (
+              comment[keywords[i][0]] &&
+              comment[keywords[i][0]].includes(keywords[i])
+            ) {
+              return comment;
+            }
+          }
+        })
+        .map(comment => {
+          const sentiment = new Sentiment();
+          const analysis = sentiment.analyze(comments[comment.id]);
+
+          switch (true) {
+            case analysis.score >= 2:
+              positiveComments.push({
+                text: comments[comment.id],
+                analysis,
+                id: comment.id
+              });
+              break;
+            case analysis.score <= -2:
+              negativeComments.push({
+                text: comments[comment.id],
+                analysis,
+                id: comment.id
+              });
+              break;
+            default:
+              neutralComments.push({
+                text: comments[comment.id],
+                analysis,
+                id: comment.id
+              });
+          }
+        });
+      this.setState({
+        positiveComments,
+        neutralComments,
+        negativeComments
+      });
+      resolve();
+    });
   }
 
-  filter() {
-    let positiveComments = [];
-    let neutralComments = [];
-    let negativeComments = [];
-    const keywords = this.state.value.replace(/ /, "").split(",");
-    this.state.comments
-      .filter(comment => {
-        for (var i = 0; i < keywords.length; i++) {
-          if (
-            comment[keywords[i][0]] &&
-            comment[keywords[i][0]].includes(keywords[i])
-          ) {
-            return comment;
-          }
-        }
-      })
-      .map(comment => {
-        const sentiment = new Sentiment();
-        const score = sentiment.analyze(comments[comment.id]).score;
-
-        switch (true) {
-          case score >= 2:
-            positiveComments.push({ comment: comments[comment.id], score });
-            break;
-          case score <= -2:
-            negativeComments.push({ comment: comments[comment.id], score });
-            break;
-          default:
-            neutralComments.push({ comment: comments[comment.id], score });
-        }
-      });
-    this.setState({
-      positiveComments,
-      neutralComments,
-      negativeComments
-    });
+  numberOfResults() {
+    return (
+      this.state.positiveComments.length +
+      this.state.negativeComments.length +
+      this.state.neutralComments.length
+    );
   }
 
   onChange(event) {
     this.setState({ value: event.target.value });
+  }
+
+  onSearch(event) {
+    event.preventDefault();
+    if (this.state.value) {
+      const t0 = performance.now();
+      this.filter().then(() => {
+        const t1 = performance.now();
+        this.setState({ searchTime: (t1 - t0).toFixed(2) });
+      });
+    } else {
+      window.alert("You need to enter a search term!");
+    }
+  }
+
+  percentage(value) {
+    return Math.floor((value / this.numberOfResults()) * 100);
   }
 }
--- src/comment.js
+++ src/comment.js
@@ -1,18 +1,48 @@
 import React from "react";
 import "./comment.css";
 
-export default class Comment extends React.Component<{ comment: String }> {
+export default class Comment extends React.Component {
   render() {
+    const comment = this.props.comment.text;
+    const score = this.props.comment.analysis.score;
+
     return (
       <div className="comment">
-        <div className="score">
-          <p>{"Score:"}</p>
-          <p style={{ color: "blue", "padding-left": 6 }}>
-            {this.props.comment.score}
-          </p>
-        </div>
-        <p className="comment-text">{this.props.comment.comment}</p>
+        <span className="comment-header">
+          Score:
+          <span style={{ color: "blue", "padding-left": 6 }}>{score}</span>
+        </span>
+        {!this.props.revealTerms ? (
+          <p className="comment-text">{comment}</p>
+        ) : (
+          <p
+            className="comment-text"
+            dangerouslySetInnerHTML={this.revealTerms()}
+          />
+        )}
       </div>
     );
+  }
+
+  revealTerms() {
+    let comment = this.props.comment.text;
+    const { positive, negative } = this.props.comment.analysis;
+
+    positive.map(term => {
+      var regEx = new RegExp(`\\b${term}\\b`, "ig");
+      comment = comment.replace(
+        regEx,
+        string => `<span class="positive-text">` + string + "</span>"
+      );
+    });
+    negative.map(term => {
+      var regEx = new RegExp(`\\b${term}\\b`, "ig");
+      comment = comment.replace(
+        regEx,
+        string => `<span class="negative-text">` + string + "</span>"
+      );
+    });
+
+    return { __html: comment };
   }
 }
